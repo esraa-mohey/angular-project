6a56d54778e076e3ea19ae086390e60f7ff13c8e
esraa-mohey <60808561+esraa-mohey@users.noreply.github.com>
2023-10-26 13:51:32 +0300

    Add files via upload

---
+++ Pipes/search.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Product } from '../Interfaces/product';
+
+@Pipe({
+  name: 'search'
+})
+export class SearchPipe implements PipeTransform {
+
+  transform(products: any[], searchTerm: string): any[] {
+    return products.filter((product) => product.title.toLowerCase().includes(searchTerm.toLowerCase()));
+  }
+
+}
